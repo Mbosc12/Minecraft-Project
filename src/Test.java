3152374f504cc9bdfeda113cfe6af3b598f6e8e3
Claude Crubilhe <ccrubilh@pcalb-mm0000.univ-jfc.fr>
2019-03-27 12:17:55 +0100

    Intégration de la feuille CSS et de la zone du bas

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,5 @@
+import java.awt.BorderLayout;
+
 import javax.print.DocFlavor.URL;
 
 import javafx.application.Application;
@@ -9,18 +11,27 @@ import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
-
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Test extends Application {
 
 	public BorderPane top = new BorderPane();
+	public BorderPane bottom = new BorderPane();
 	public GridPane g1 = new GridPane();
-
+	
+	final FlowPane barreDuBas = new FlowPane(); 
+	
 	public static void main(String[] args) {
-
+		Scene scene = new Scene(new Group(), 500, 400);
+		
+		scene.getStylesheets().add("application.css");
+		
+		
 		Application.launch(Test.class, args);
 
 		Test v = new Test();
@@ -29,15 +40,15 @@ public class Test extends Application {
 		Block planche = new Block(0, "Planche", false, new Craft(4));
 		planche.craft.setCraft("---", "-#-", "---");
 		planche.craft.setElement(1, 1, buche);
-
 	}
-
 	@Override
 	public void start(Stage primaryStage) {
 		Group root = new Group();
 		Scene scene = new Scene(root, 500, 500);
 		primaryStage.setScene(scene);
-
+		String css = getClass().getResource("application.css").toExternalForm();
+		scene.getStylesheets().add(css);
+		
 		/*java.net.URL cssURL = getClass().getResource("application.css");
 		if (cssURL != null) {
 		  scene.getStylesheets().add(cssURL.toExternalForm());
@@ -47,7 +58,6 @@ public class Test extends Application {
 		for (int i = 1; i<4; i++) {
 			for (int j = 0; j<3; j++) {
 				Button b = new Button();
-
 				b.setTextFill(null);
 				b.setText("Button"+a); a+= 1;
 				b.setPrefSize(80,80);
@@ -61,10 +71,13 @@ public class Test extends Application {
 					@Override 
 					public void handle(ActionEvent actionEvent) { 
 						System.out.println(b.getText()); 
+						bottom.setVisible(true);
 					} 
 				});
 			}
 		}
+		
+		 
 
 		
 		
@@ -93,6 +106,17 @@ public class Test extends Application {
 		top.setPadding(new Insets(80, 5, 5, 100));
 		root.getChildren().add(top);
 
+		
+		//bottom.setPadding(new Insets(10,400,5,100));
+		barreDuBas.setId("barreDuBas");
+		barreDuBas.setPrefSize(500, 100);
+		bottom.setLayoutX(0);
+		bottom.setLayoutY(400);
+	
+		bottom.setBottom(barreDuBas);
+		bottom.setVisible(false);
+		root.getChildren().add(bottom);
+		
 		primaryStage.show();
 	}
 
